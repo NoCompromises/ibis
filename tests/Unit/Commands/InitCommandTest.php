c323b4e369be40db82bc23ad4e345c04365366a7
Aaron Saray <me@aaronsaray.com>
2021-03-20 17:23:39 -0500

    First unit test actually running the symfony commands.

--- tests/Unit/Commands/InitCommandTest.php
+++ tests/Unit/Commands/InitCommandTest.php
@@ -2,12 +2,24 @@
 
 namespace Tests\Unit\Commands;
 
+use Ibis\Commands\InitCommand;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
 
 class InitCommandTest extends TestCase
 {
-    public function testInit(): void
+    public function testInitEndsEarlyIfAssetsFolderExists(): void
     {
-        self::assertTrue(true);
+        chdir(__DIR__.'/../../Mocks/AssetsExist');
+
+        $application = new Application();
+        $application->add(new InitCommand());
+        $command = $application->find('init');
+        $commandTester = new CommandTester($command);
+
+        $commandTester->execute([]);
+        self::assertStringContainsString('Project already initialised!', $commandTester->getDisplay());
+        self::assertEquals(0, $commandTester->getStatusCode());
     }
 }
